784209824f2ba23cfd3638296d50531b5963fd44
TopShelfBullard <bullardmitchell@gmail.com>
2016-10-22 02:16:59 -0500

    Add note display_name and wire up a show page.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,12 @@
 class Note < ActiveRecord::Base
+  def display_name
+    "#{letter.upcase}#{display_accidental}"
+  end
+
+  private
+
+  def display_accidental
+    return "" unless flat || sharp
+    flat ? " Flat" : " Sharp"
+  end
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    [
+      [["cb", 12], ["c", 1 ], ["c#", 2 ]],
+      [["db", 2 ], ["d", 3 ], ["d#", 4 ]],
+      [["eb", 4 ], ["e", 5 ], ["e#", 6 ]],
+      [["fb", 5 ], ["f", 6 ], ["f#", 7 ]],
+      [["gb", 7 ], ["g", 8 ], ["g#", 9 ]],
+      [["ab", 9 ], ["a", 10], ["a#", 11]],
+      [["bb", 11], ["b", 12], ["b#", 1 ]]
+    ].each do |note_letter_group|
+      flat_note, note, sharp_note = note_letter_group
+      flat_name, flat_value = flat_note
+      name, value = note
+      sharp_name, sharp_value = sharp_note
+
+      Note.create!(name: flat_name,  letter: name, value: flat_value,  flat: true )
+      Note.create!(name: name,       letter: name, value: value                   )
+      Note.create!(name: sharp_name, letter: name, value: sharp_value, sharp: true)
+    end
+  end
+
+  def test_display_name
+    Note.all.each do |note|
+      expected = note.letter.upcase
+      expected = "#{expected} Flat"  if note.flat
+      expected = "#{expected} Sharp" if note.sharp
+      assert_equal expected, note.display_name, "Expected '#{expected}', but got #{note.display_name}."
+    end
+  end
 end
